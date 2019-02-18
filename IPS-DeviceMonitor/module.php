36f5635f604bb3e61abd2806b2d26a09994ccc21
Kai Schnittcher <kai.schnittcher@gmail.com>
2019-02-18 12:36:39 +0100

    Version 1.1 WOL hinzugefügt

--- IPS-DeviceMonitor/module.php
+++ IPS-DeviceMonitor/module.php
@@ -13,30 +13,36 @@ class IPS_DeviceMonitor extends IPSModule
         parent::Create();
 
         $this->RegisterPropertyString('IPAddress', '');
+        $this->RegisterPropertyString('BroadcastAddress', '');
+        $this->RegisterPropertyString('MACAddress', '');
         $this->RegisterPropertyInteger('PingTimeout', 1000);
         $this->RegisterPropertyInteger('Interval', 20);
 
         $this->RegisterTimer('DM_UpdateTimer', 0, 'DM_UpdateStatus($_IPS[\'TARGET\']);');
+        $this->RegisterVariablenProfiles();
+        $this->RegisterVariableBoolean('DeviceStatus', 'Status', 'DM.Status');
+        $this->RegisterVariableInteger('DeviceWOL', 'Wake On Lan', 'DM.WOL');
+        SetValue($this->GetIDForIdent('DeviceWOL'), 1);
+        $this->EnableAction('DeviceWOL');
     }
 
     public function Destroy()
     {
-        $this->UnregisterTimer('DM_UpdateStatus');
+        //$this->UnregisterTimer('DM_UpdateStatus');
     }
 
     public function ApplyChanges()
     {
         //Never delete this line!
         parent::ApplyChanges();
-        $this->RegisterVariablenProfiles();
-        $this->RegisterVariableBoolean('DeviceStatus', 'Status', 'DM.Status');
+
         $this->SetTimerInterval('DM_UpdateTimer', $this->ReadPropertyInteger('Interval') * 1000);
     }
 
     public function UpdateStatus()
     {
-        if ((($this->ReadPropertyString('IPAddress') != '') and $this->ReadPropertyString('PingTimeout') != '')) {
-            if (@Sys_Ping($this->ReadPropertyString('IPAddress'), $this->ReadPropertyString('PingTimeout'))) {
+        if ((($this->ReadPropertyString('IPAddress') != '') and $this->ReadPropertyInteger('PingTimeout') != '')) {
+            if (@Sys_Ping($this->ReadPropertyString('IPAddress'), $this->ReadPropertyInteger('PingTimeout'))) {
                 SetValue($this->GetIDForIdent('DeviceStatus'), true);
             } else {
                 SetValue($this->GetIDForIdent('DeviceStatus'), false);
@@ -47,9 +53,33 @@ class IPS_DeviceMonitor extends IPSModule
     private function RegisterVariablenProfiles()
     {
         //Profile for Online / Offline Status
-        $this->RegisterProfileBooleanEx('DM.Status', 'Network', '', '', array(
-            array(false, 'Offline',  '', 0xFF0000),
-            array(true, 'Online',  '', 0x00FF00),
-        ));
+        if (!IPS_VariableProfileExists('DM.Status')) {
+            $this->RegisterProfileBooleanEx('DM.Status', 'Network', '', '', array(
+                array(false, 'Offline',  '', 0xFF0000),
+                array(true, 'Online',  '', 0x00FF00),
+            ));
+        }
+
+        //Profile for WOL
+        if (!IPS_VariableProfileExists('DM.WOL')) {
+            $Associations = array();
+            $Associations[] = array(1, 'Start', '', -1);
+            $this->RegisterProfileIntegerEx('DM.WOL', 'Network', '', '', $Associations);
+        }
+    }
+
+    public function RequestAction($Ident, $Value)
+    {
+        $this->SendDebug(__FUNCTION__ . ' Ident', $Ident, 0);
+        $this->SendDebug(__FUNCTION__ . ' Value', $Value, 0);
+        switch ($Ident) {
+            case 'DeviceWOL':
+                $this->SendDebug(__FUNCTION__, 'Device wakeup', 0);
+                $this->WakeOnLan();
+                break;
+            default:
+                $this->SendDebug(__FUNCTION__, 'Undefined Ident', 0);
+                break;
+        }
     }
 }
--- libs/helper.php
+++ libs/helper.php
@@ -34,4 +34,62 @@ trait helper
             IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
         }
     }
+
+    protected function RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, $StepSize)
+    {
+        if (!IPS_VariableProfileExists($Name)) {
+            IPS_CreateVariableProfile($Name, 1);
+        } else {
+            $profile = IPS_GetVariableProfile($Name);
+            if ($profile['ProfileType'] != 1) {
+                throw new Exception('Variable profile type does not match for profile ' . $Name);
+            }
+        }
+        IPS_SetVariableProfileIcon($Name, $Icon);
+        IPS_SetVariableProfileText($Name, $Prefix, $Suffix);
+        IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
+    }
+    protected function RegisterProfileIntegerEx($Name, $Icon, $Prefix, $Suffix, $Associations)
+    {
+        if (count($Associations) === 0) {
+            $MinValue = 0;
+            $MaxValue = 0;
+        } else {
+            $MinValue = $Associations[0][0];
+            $MaxValue = $Associations[count($Associations) - 1][0];
+        }
+        $this->RegisterProfileInteger($Name, $Icon, $Prefix, $Suffix, $MinValue, $MaxValue, 0);
+        foreach ($Associations as $Association) {
+            IPS_SetVariableProfileAssociation($Name, $Association[0], $Association[1], $Association[2], $Association[3]);
+        }
+    }
+
+    protected function WakeOnLan()
+    {
+
+        $addr = $this->ReadPropertyString('BroadcastAddress');
+        $addr_byte = explode(':', $this->ReadPropertyString('MACAddress'));
+        $hw_addr = '';
+        for ($a=0; $a < 6; $a++) $hw_addr .= chr(hexdec($addr_byte[$a]));
+        $msg = chr(255).chr(255).chr(255).chr(255).chr(255).chr(255);
+        for ($a = 1; $a <= 16; $a++) $msg .= $hw_addr;
+        // send it to the broadcast address using UDP
+        $s = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        if ($s == false)
+        {
+            $this->SendDebug('Error creating socket!','Error code is '.socket_last_error($s).' - ' . socket_strerror(socket_last_error($s)),0);
+        }
+        else
+        {
+            // setting a broadcast option to socket:
+            $opt_ret = socket_set_option($s, 1, 6, TRUE);
+            if($opt_ret < 0)
+            {
+                $this->SendDebug('setsockopt() failed, error:' , strerror($opt_ret),0);
+            }
+            $result = socket_sendto($s, $msg, strlen($msg), 0, $addr, 2050);
+            socket_close($s);
+            $this->SendDebug('Result' , 'Magic Packet sent ('.$result.') to '.$addr.', MAC='.$this->ReadPropertyString('MACAddress'),0);
+        }
+    }
 }
